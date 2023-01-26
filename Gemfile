c95a5494f8b6ab412025094be2adc1c95f68b9fd
Shisui <37116322+Shisui6@users.noreply.github.com>
2023-01-26 15:48:13 +0100

    Add template files

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+# Gem dependencies
+
+gem 'rake', '~> 13.0'
+
+gem 'minitest', '~> 5.0'
+
+gem 'rubocop', '>= 1.0', '< 2.0'
---
+++ lib/program.rb
@@ -0,0 +1,9 @@
+# The coolest program
+class CoolProgram
+  attr_reader :coolness
+  def initialize
+    @coolness = 11
+  end
+end
+
+puts "Coolness: #{CoolProgram.new.coolness}/10"
